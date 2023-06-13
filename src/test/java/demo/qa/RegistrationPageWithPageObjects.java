54230092799c971522f4fd27adf27746edddb845
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-06-13 16:41:52 +0300

    minor tweaks

--- src/test/java/demo/qa/RegistrationPageWithPageObjects.java
+++ src/test/java/demo/qa/RegistrationPageWithPageObjects.java
@@ -12,39 +12,35 @@ import static com.codeborne.selenide.Selenide.*;
 public class RegistrationPageWithPageObjects extends TestBase {
 
     RegistrationPage registrationPage = new RegistrationPage();
-        @Test
 
-        void successTest() {
-            registrationPage.openPage()
-          .setFirstName("Vasya")
-          .setLastName("Pupkin")
-                    .setEmail("vasya@punpkin.ru")
-         .setGender("Male")
-                    .setNumber("2741001274")
-         .setBirthday("July","2008","1");
-
-            $("#subjectsInput").setValue("E");
-            $(byText("English")).click();
-            $("#hobbiesWrapper").$(byText("Reading")).click();
-            $("#currentAddress").setValue("Kazansky vokzal");
-            //$("#uploadPicture").uploadFromClasspath("Java.png");
-            $("#stateCity-wrapper").$("#state").click();
-            $("#state").$(byText("NCR")).click();
-            $("#stateCity-wrapper").$("#city").click();
-            $("#city").$(byText("Gurgaon")).click();
-            $("#submit").click();
+    @Test
+    void successTest() {
+        registrationPage.openPage()
+                .setFirstName("Vasya")
+                .setLastName("Pupkin")
+                .setEmail("vasya@punpkin.ru")
+                .setGender("Male")
+                .setNumber("2741001274")
+                .setBirthday("July", "2008", "1")
+                .setSubjectInput("English")
+                .setHobbies("Reading")
+                .uploadPicture("Java.png")
+                .setCurrentAdress("Kazansky Vokzal")
+                .setState("NCR")
+                .setCity("Gurgaon");
+        $("#submit").click();
 
 
-            $(".table-responsive").shouldHave(text("Vasya Pupkin"));
-            $(".table-responsive").shouldHave(text("vasya@pupkin.com"));
-            $(".table-responsive").shouldHave(text("Male"));
-            $(".table-responsive").shouldHave(text("2741001274"));
-            $(".table-responsive").shouldHave(text("1 June,2023"));
-            $(".table-responsive").shouldHave(text("English"));
-            $(".table-responsive").shouldHave(text("Kazansky vokzal"));
-            $(".table-responsive").shouldHave(text("NCR"));
-            $(".table-responsive").shouldHave(text("Gurgaon"));
+        $(".table-responsive").shouldHave(text("Vasya Pupkin"));
+        $(".table-responsive").shouldHave(text("vasya@pupkin.com"));
+        $(".table-responsive").shouldHave(text("Male"));
+        $(".table-responsive").shouldHave(text("2741001274"));
+        $(".table-responsive").shouldHave(text("1 June,2023"));
+        $(".table-responsive").shouldHave(text("English"));
+        $(".table-responsive").shouldHave(text("Kazansky vokzal"));
+        $(".table-responsive").shouldHave(text("NCR"));
+        $(".table-responsive").shouldHave(text("Gurgaon"));
 
 
-        }
     }
+}
--- src/test/java/demo/qa/pages/RegistrationPage.java
+++ src/test/java/demo/qa/pages/RegistrationPage.java
@@ -11,41 +11,90 @@ public class RegistrationPage {
 
     CalendarComponent calendarComponent = new CalendarComponent();
 
-SelenideElement firstNameInput = $("#firstName"),
- secondNameInput =$("#lastName"),
-userEmail =  $("#userEmail"),
- genderWrapper =   $("#genterWrapper"),
+    SelenideElement firstNameInput = $("#firstName"),
+            secondNameInput = $("#lastName"),
+            userEmail = $("#userEmail"),
+            genderWrapper = $("#genterWrapper"),
+            setNumber = $("#userNumber"),
+            birthDayInput = $("#dateOfBirthInput"),
+            subjectLetterInput = $("#subjectsWrapper").$("#subjectsInput"),
+            subjectChooseInput = $("#subjectsWrapper").$("#subjectsInput"),
+            setHobbiesInput = $("#hobbiesWrapper"),
+            uploadPictureInput = $("#uploadPicture"),
+            currentAddressInput = $("#currentAddress"),
+            stateInput = $("#stateCity-wrapper").$("#state"),
+            cityInput = $("#stateCity-wrapper").$("#city");
 
-    setNumber =  $("#userNumber"),
-
-           birthDayInput = $("#dateOfBirthInput") ;
-
-    public RegistrationPage openPage(){
+    public RegistrationPage openPage() {
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         return this;
-        }
+    }
 
-public RegistrationPage setFirstName(String value){
-    firstNameInput.setValue(value);
-    return this;}
-public RegistrationPage setLastName(String value){
+    public RegistrationPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setLastName(String value) {
         secondNameInput.setValue(value);
-    return this;}
-public RegistrationPage setEmail(String value){
+        return this;
+    }
+
+    public RegistrationPage setEmail(String value) {
         userEmail.setValue(value);
-    return this;}
-public RegistrationPage setGender(String value){
-        genderWrapper.$(byText(value)).click();;
-    return this;}
-    public RegistrationPage setNumber (String value){
+        return this;
+    }
+
+    public RegistrationPage setGender(String value) {
+        genderWrapper.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage setNumber(String value) {
         setNumber.setValue(value);
-        return this;}
-    public RegistrationPage setBirthday (String month, String year, String day){
+        return this;
+    }
+
+    public RegistrationPage setBirthday(String month, String year, String day) {
         birthDayInput.click();
         calendarComponent.setDate(month, year, day);
-    return this;}
+        return this;
+    }
+
+   // public RegistrationPage setSubjectLetter(String value) {
+    //    subjectLetterInput.setValue(value);
+   //     return this;
+ //   }
+
+    public RegistrationPage setSubjectInput(String value) {
+        subjectChooseInput.setValue(value).pressEnter();
+        return this;
+    }
+
+    public RegistrationPage setHobbies(String value) {
+        setHobbiesInput.$(byText(value)).click();
+        return this;
+    }
+
+    public RegistrationPage uploadPicture(String value) {
+        uploadPictureInput.uploadFromClasspath(value);
+        return this;
+    }
+
+    public RegistrationPage setCurrentAdress(String value) {
+        currentAddressInput.setValue(value);
+        return this;
+    }
+    public RegistrationPage setState(String value) {
+        stateInput.setValue(value);
+        return this;
+    }
+
+    public RegistrationPage setCity(String value) {
+        cityInput.setValue(value);
+        return this;}
 
 
 }
