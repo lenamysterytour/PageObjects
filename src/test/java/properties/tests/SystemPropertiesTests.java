c4b37b0bac0840433cdf1b86d51bf3324bf0633a
Bolotnikova Elena <lenamysterytour@gmail.com>
2023-08-12 17:16:12 +0300

    added-hellotest

--- src/test/java/properties/tests/SystemPropertiesTests.java
+++ src/test/java/properties/tests/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package properties.tests;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Tag("property")
@@ -10,5 +12,15 @@ public class SystemPropertiesTests {
     void systemPropertiesTest(){
         String browser = System.getProperty("browser","mozilla");
         System.out.println(browser);
+    }
+
+    @Tag("hello")
+    @Test
+    void systemProperties1Test(){
+        String name = System.getProperty("name","default student");
+        String message = format("Hello, %s!", name);
+        System.out.println(message);
+
+        // gradle hello_test - Dname="Elena Bolotnikova"
     }
 }
